ce47130a50dce6ff33d71d1b91fa5593460dcdb8
Tanojsaipraveen <47939411+tanojsaipraveen@users.noreply.github.com>
2025-11-30 09:31:02 +0530

    Update CACHE_NAME and simplify cache handling

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tracker-pwa-v8';
+const CACHE_NAME = 'tracker-pwa-v9';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,22 +9,12 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
